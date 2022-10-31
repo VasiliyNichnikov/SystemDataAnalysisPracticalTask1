925e4821f7d65959c8f50f83956d37fd6fa598af
v.nichnikov <v.nichnikov@vk.team>
2022-10-31 23:10:29 +0300

    Создан граф рисования и создания запрос

---
+++ PracticalTask1/Utils/GraphDrawer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using PracticalTask1.Algorithms;
+using QuickGraph;
+using QuickGraph.Graphviz;
+using QuickGraph.Graphviz.Dot;
+
+namespace PracticalTask1.Utils
+{
+    public class GraphDrawer
+    {
+        private readonly DeepFindSearchGraph<EmployeeBase> _dfsEmployee;
+        private readonly IReadOnlyList<DepartmentBase> _departments;
+        private readonly IReadOnlyList<EmployeeBase> _staff;
+
+        public GraphDrawer(IReadOnlyList<EmployeeBase> staff, IReadOnlyList<DepartmentBase> departments)
+        {
+            _dfsEmployee = new DeepFindSearchGraph<EmployeeBase>();
+            _staff = staff;
+            _departments = departments;
+            // var edges = dfsEmployee.TranslateToEdgesForGraphWithDepartments(departments as IReadOnlyList<DepartmentBase>);
+        }
+
+        public void Draw(TypeSearch type)
+        {
+            // todo тут проблема, что у нас обязательно 4 элемент должен быть главным в графе
+            
+            // todo имеено тут работники лишаются отдела
+            var nodes = _dfsEmployee.FoundAllNodes(_staff[4],new List<EmployeeBase>(), type);
+
+            var edges = new List<Edge<Vertex>>();
+
+            Console.WriteLine("Found users:");
+            foreach (var node in nodes)
+            {
+                Console.WriteLine($"Name: {node.Name}. Type: {node.GetType()}");
+                
+                var elements = _dfsEmployee.TranslateToEdgesForGraphWithStartDepartment(node, _departments);
+                edges.AddRange(elements);
+            }
+            
+            var graph = edges.ToAdjacencyGraph<Vertex, Edge<Vertex>>();
+            var algo = new GraphvizAlgorithm<Vertex, Edge<Vertex>>(graph)
+            {
+                ImageType = GraphvizImageType.Gd2
+            };
+            algo.FormatVertex += (sender, eventArgs) =>
+            {
+                var selectedVertex = eventArgs.Vertex;
+                eventArgs.VertexFormatter.Label = selectedVertex.Name;
+                eventArgs.VertexFormatter.StrokeColor = selectedVertex.StrokeColor;
+            };
+            
+            var pathDot = PathHandler.GetGraphDotPath();
+            algo.Generate(new FileDotEngine(), pathDot);
+            
+            // Сохранение в Png
+            using(StreamReader readText = new StreamReader(pathDot))
+            {
+                string graphVizString = readText.ReadToEnd();
+                Bitmap bm = MyFileDotEngine.Run(graphVizString);
+                bm.Save(PathHandler.GetPathImage("GraphImage"));
+            }
+        }
+        
+    }
+}
---
+++ PracticalTask1/Utils/RequestCollector.cs
@@ -0,0 +1,73 @@
+﻿namespace PracticalTask1.Utils
+{
+    public class RequestCollector
+    {
+        /// <summary>
+        /// Добавление имени в поиск
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public RequestCollector AddName(string name)
+        {
+            EmployeeBase.SelectSearchField("_name", name);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Добавление фамилии в поиск
+        /// </summary>
+        /// <param name="surname"></param>
+        /// <returns></returns>
+        public RequestCollector AddSurname(string surname)
+        {
+            EmployeeBase.SelectSearchField("_surname", surname);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Добавление возвраста в поиск
+        /// </summary>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        public RequestCollector AddAge(int age)
+        {
+            EmployeeBase.SelectSearchField("_age", age);
+            
+            return this;
+        }
+
+        /// <summary>
+        /// Добавить зарплату в поиск
+        /// </summary>
+        /// <param name="salary"></param>
+        /// <returns></returns>
+        public RequestCollector AddSalary(float salary)
+        {
+            EmployeeBase.SelectSearchField("_salary", salary);
+            
+            return this;
+        }
+
+        /// <summary>
+        /// Добавить номер телефона в поиск
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns></returns>
+        public RequestCollector AddPhoneNumber(long phoneNumber)
+        {
+            EmployeeBase.SelectSearchField("_phoneNumber", phoneNumber);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Очистка поиска
+        /// </summary>
+        public void ClearSearch()
+        {
+            EmployeeBase.ClearSearch();
+        }
+    }
+}
